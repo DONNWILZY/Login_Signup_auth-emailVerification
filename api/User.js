7fa63e646cac978172ece69d0c20139b68f4b9ed
donnwilzy <ask4godswill@gmail.com>
2023-01-30 23:57:34 +0100

    user route setup

--- api/User.js
+++ api/User.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 //signup route
 router.post('/signup', (req, res)=>{
-   
+   let {name, email, password, dateOfBirth, phone} = req.body;
+   name = name.trim();
+   email = email.trim();
+   password = password.trim();
+   dateOfBirth = dateOfBirth.trim();
+   phone = phone.trim();
+
+   if (name == '' || email == '' || password == '' || dateOfBirth == '' || phone ==''){
+        res.status(402).json("invalid credntials");
+   }
 })
 
 
