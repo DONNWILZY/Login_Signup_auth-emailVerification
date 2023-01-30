1a68b2ecb4b602814c055bce5cf0aab113ffdf7e
donnwilzy <ask4godswill@gmail.com>
2023-01-30 23:03:09 +0100

    db conncted

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const PORT = 3001;
+const dotenv = require('dotEnv')
+const PORT = 8080;
 const mongoose = require('mongoose')
-app.express();
+const bodyParser = require('body-parser');
+app = express();
+app.use(bodyParser);
 
 // //database connection
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true});
 const db = mongoose.connection;
 db.on('error', (error)=> console.error(error));
@@ -12,7 +16,7 @@ db.once('open', ()=>console.log('db connected'));
 
 
 app.listen(PORT, ()=>{
-    console.log(`Conneted to ${PORT} 127.0.0.2:${PORT}`)
+    console.log(`Conneted to ${PORT} 127.0.0.1:${PORT}`)
 })
 
 
